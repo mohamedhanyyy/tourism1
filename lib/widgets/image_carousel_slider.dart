712a8fda585a052e84613186374bc4e4e2e926a8
Momen Mahmoud Farid <86335146+MomenF@users.noreply.github.com>
2022-03-26 12:49:49 +0200

    remove conflicts

--- lib/widgets/image_carousel_slider.dart
+++ lib/widgets/image_carousel_slider.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_controller.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
